f3235b9c39a57cd1ebde7fa699d8a4ae27f80cf1
Gummadavelli Bhanu Teja <bhanutejagummadevelli@gmail.com>
2025-05-15 02:17:15 +0530

    Create Left View of Tree.java
    Signed-off-by: Gummadavelli Bhanu Teja <bhanutejagummadevelli@gmail.com>

---
+++ Left View of Tree.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static class Node{
+        int data;
+        Node left,right;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static Node insert(Node root,int val){
+        if(root==null){ 
+            return new Node(val);
+        }
+        if(val<root.data) root.left=insert(root.left,val);
+        else root.right=insert(root.right,val);
+        return root;
+    }
+    public static List<Integer>leftview(Node root){
+        List<Integer> res=new ArrayList<>();
+        recurleft(root,0,res);
+        return res;
+    }
+    public static void recurleft(Node root,int level,List<Integer> res){
+        if(root==null) return;
+        if(res.size()==level) res.add(root.data);
+        recurleft(root.left,level+1,res);
+        recurleft(root.right,level+1,res);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            Node root=null;
+            for(int i=0;i<n;i++){
+                int val=sc.nextInt();
+                root=insert(root,val);
+            }
+            List<Integer> res=leftview(root);
+            for(int i:res){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
